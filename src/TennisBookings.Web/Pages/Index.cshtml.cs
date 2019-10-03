6835d507399dbedba3deeeca1d18e5637fc9ff14
Moses Byanyuma <mbyanyuma108@gmail.com>
2019-10-03 02:18:18 -0400

    Accessing configuration sections to enable random greeting and fetch current weather

--- src/TennisBookings.Web/Pages/Index.cshtml.cs
+++ src/TennisBookings.Web/Pages/Index.cshtml.cs
@@ -9,11 +9,14 @@ namespace TennisBookings.Web.Pages
     {
         private readonly IGreetingService _greetingService;
         private readonly IConfiguration _configuration;
+        private readonly IWeatherForecaster _weatherForecaster;
 
-        public IndexModel(IGreetingService greetingService, IConfiguration configuration)
+        public IndexModel(IGreetingService greetingService, IConfiguration configuration,
+            IWeatherForecaster weatherForecaster)
         {
             _greetingService = greetingService;
             _configuration = configuration;
+            _weatherForecaster = weatherForecaster;
         }
 
         public string Greeting { get; private set; }
@@ -24,10 +27,41 @@ namespace TennisBookings.Web.Pages
 
         public async Task OnGet()
         {
+            var homePageFeatures = _configuration.GetSection("Features:HomePage");
 
-            if (_configuration.GetValue<bool>("Features:HomePage:EnableGreeting"))
+            if (homePageFeatures.GetValue<bool>("EnableGreeting"))
             {
                 Greeting = _greetingService.GetRandomGreeting();
+            }
+
+            ShowWeatherForecast = homePageFeatures.GetValue<bool>("EnableWeatherForecast");
+
+            if (ShowWeatherForecast)
+            {
+                var title = homePageFeatures["ForecastSectionTitle"];
+                ForecastSectionTitle = string.IsNullOrEmpty(title) ? "How's the weather?" : title;
+
+                var currentWeather = await _weatherForecaster.GetCurrentWeatherAsync();
+
+                if (currentWeather != null)
+                {
+                    switch (currentWeather.Description)
+                    {
+                        case "Sun":
+                            WeatherDescription = "It's sunny right now. A great day for tennis!";
+                            break;
+
+                        case "Cloud":
+                            WeatherDescription = "It's cloudy at the moment and the outdoor courts are in use.";
+                            break;
+
+                        case "Rain":
+                            WeatherDescription = "We;re sorry but its raining here. No outdoor courts in use,";
+                            break;
+
+                    }
+                }
+
             }
         }
     }
