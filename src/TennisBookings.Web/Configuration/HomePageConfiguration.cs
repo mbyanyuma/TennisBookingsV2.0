493064e001bd121e7043b78e576880e3e3771fd9
Moses Byanyuma <mbyanyuma108@gmail.com>
2019-10-10 19:30:28 -0400

    created separate configurations file for our index page

---
+++ src/TennisBookings.Web/Configuration/HomePageConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TennisBookings.Web.Configuration
+{
+    public class HomePageConfiguration
+    {
+        public bool EnableGreeting { get; set; }
+        public bool EnableWeatherForecast { get; set; }
+        public string ForecastSectionTitle { get; set; }
+    }
+}
