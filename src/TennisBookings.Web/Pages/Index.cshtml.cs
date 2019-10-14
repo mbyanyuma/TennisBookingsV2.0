11f54ccd8628855240d06ecd670eb13cdcf0fb89
Moses Byanyuma <mbyanyuma108@gmail.com>
2019-10-14 09:21:17 -0400

    .

--- src/TennisBookings.Web/Pages/Index.cshtml.cs
+++ src/TennisBookings.Web/Pages/Index.cshtml.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Configuration;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Options;
 using TennisBookings.Web.Configuration;
